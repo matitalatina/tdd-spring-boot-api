d7a4b767b64929d1c4e211da7019341dd3189fd4
Mattia Natali <matitalatina@gmail.com>
2019-07-20 12:59:38 +0200

    Bootstrap project

---
+++ src/test/java/it/mattianatali/tddspringbootapi/TddSpringBootApiApplicationTests.java
@@ -0,0 +1,16 @@
+package it.mattianatali.tddspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class TddSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
